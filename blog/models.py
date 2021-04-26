9b2d07a783f70cdf7b541a03d76097b587e26b75
medalon <medalon07@gmail.com>
2021-04-26 18:28:43 +0600

    Less 2

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -12,6 +13,7 @@ class PublishedManager(models.Manager):
 class Post(models.Model):
 	objects = models.Manager() # The default manager.
 	published = PublishedManager() # Our custom manager.
+	tags = TaggableManager()
 
 	STATUS_CHOISES = (
 		('draft', 'Draft'),
@@ -38,3 +40,18 @@ class Post(models.Model):
 						args=[self.publish.year,
 						self.publish.month,
 						self.publish.day, self.slug])
+						
+class Comment(models.Model):
+	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+	name = models.CharField(max_length=80)
+	email = models.EmailField()
+	body = models.TextField()
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	active = models.BooleanField(default=True)
+
+	class Meta:
+		ordering = ('created',)
+	
+	def __str__(self):
+		return f'Comment by {self.name} on {self.post}'
--- serepkg/settings.py
+++ serepkg/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'kabar.apps.KabarConfig',
 	'blog.apps.BlogConfig',
+	'taggit',
 ]
 
 MIDDLEWARE = [
